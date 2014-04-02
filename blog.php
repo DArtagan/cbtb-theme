3c104db2b2b850f29aa2377d0a096234e56a0871
Willy Weiskopf <william@weiskopf.me>
2014-04-02 14:52:35 -0600

    Full content, rather than excerpt.

--- blog.php
+++ blog.php
@@ -17,7 +17,7 @@ get_header(); ?>
 		      <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
             <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
             <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-            <?php the_excerpt(); ?>
+            <?php the_content(); ?>
           </div>
           <?php endwhile; ?>
           <div class="pagination">
